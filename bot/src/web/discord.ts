b0d0f25d40111f7ec12cddbcc783b4674156c4d3
didithilmy <didithilmy@gmail.com>
2020-07-19 18:30:19 +0700

    Trim long name

--- bot/src/web/discord.ts
+++ bot/src/web/discord.ts
@@ -13,6 +13,19 @@ export async function onboardUser(guildId: string, userId: string, assignedNickn
     const member = guild.members.resolve(userId);
     const role = guild.roles.cache.find(role => role.name === assignedRole);
 
-    await member.setNickname(assignedNickname);
+    const trimmedFullName = trimFullName(assignedNickname, 32);
+
+    await member.setNickname(trimmedFullName);
     await member.roles.add(role);
+}
+
+function trimFullName(name: string, length: number) {
+    if (name.length <= length) {
+        return name;
+    } else {
+        const trimmed = name.substring(0, length);
+        const words = trimmed.split(' ');
+        words[words.length - 1] = words[words.length - 1].charAt(0);
+        return words.join(' ');
+    }
 }
